48687b4b3315d0c04bb37e62703b762227606b2b
ashfakshibli <shibli.emon@gmail.com>
2022-11-20 11:30:48 -0500

    Adding git repo

---
+++ autoAnalyseApps.py
@@ -0,0 +1,47 @@
+import os
+import getpass
+import subprocess
+
+ 
+current_file_path = os.getcwd()
+current_user = getpass.getuser()
+
+apps_dir_path = str(current_file_path) + '/Apps'
+
+app_list = []
+
+for path in os.listdir(apps_dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(apps_dir_path, path)):
+        app_list.append(path)
+print(app_list)
+
+# Construct commands for running Flowdroid on each file.
+
+command_dict = {}
+for app in app_list:
+    command = ["java", "-jar", "soot-infoflow-cmd-jar-with-dependencies.jar", "-a"]
+    command.append("Apps/"+ app) 
+    command.append("-p")
+    command.append("/Users/"+current_user+"/Library/Android/sdk/platforms")
+    command.append("-s")
+    command.append("SourcesAndSinks.txt")
+    command_dict[app] = command
+
+print(command_dict)
+
+
+path = "Analysis"
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
+
+for app, command in command_dict.items():
+    file = "Analysis/"+app+".txt"
+    print(file, command)
+    with open(file, "w") as f:
+        p = subprocess.Popen(command, stdout=f)
+        p.communicate()
+    f.close()
+
