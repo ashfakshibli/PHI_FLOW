147eb3a7a7e7dd4aa86eb47ebd439886ea552292
ashfakshibli <shibli.emon@gmail.com>
2022-12-09 00:28:42 -0500

    Flowdroid analysis on Med Apps

--- autoAnalyseApps.py
+++ autoAnalyseApps.py
@@ -6,7 +6,7 @@ import subprocess
 current_file_path = os.getcwd()
 current_user = getpass.getuser()
 
-apps_dir_path = str(current_file_path) + '/Apps'
+apps_dir_path = str(current_file_path) + '/Apps/Medicalapps'
 
 app_list = []
 
@@ -21,7 +21,7 @@ for path in os.listdir(apps_dir_path):
 command_dict = {}
 for app in app_list:
     command = ["java", "-jar", "soot-infoflow-cmd-jar-with-dependencies.jar", "-a"]
-    command.append("Apps/"+ app) 
+    command.append("Apps/Medicalapps/"+ app) 
     command.append("-p")
     command.append("/Users/"+current_user+"/Library/Android/sdk/platforms")
     command.append("-s")
@@ -36,10 +36,11 @@ path = "Analysis"
 if not os.path.exists(path):
     os.makedirs(path)
 
-
+COUNT = 0
 for app, command in command_dict.items():
     file = "Analysis/"+app+".txt"
-    print(file, command)
+    print(file, COUNT)
+    COUNT+=1
     with open(file, "w") as f:
         p = subprocess.Popen(command, stderr=f)
         p.communicate()
